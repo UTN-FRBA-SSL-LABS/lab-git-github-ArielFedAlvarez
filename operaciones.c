27f1fb39a9e8320a3cf60553ad1adf4027e4e0de
guadaasu <gasurmendi@frba.utn.edu.ar>
2026-05-02 18:06:56 -0300

    Agrego comentario explicativo solicitado por el owner

--- operaciones.c
+++ operaciones.c
@@ -1,7 +1,7 @@
 #include "operaciones.h"
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
-
+// La funcion sumar recibe dos enteros y retorna la suma entre ellos
 int sumar(const int a,const int b) { //se agrega const a las variantes que no se modifican
     return a + b;
 }
